5c55de9c17cf5b535f899599cca71041db3c0d99
Kieu Duc <kieuduc223@gmail.com>
2026-02-06 17:47:09 +0700

    video 21

---
+++ 21_Console/video21.js
@@ -0,0 +1,13 @@
+console.log("Log");
+console.error("Error");
+console.warn("Warning");
+
+let name = "Duc";
+let age = 22;
+console.log("Name: " + name + ", age: " + age);
+console.log("Name:", name, ", age:", age);
+console.log("%cStop", "color: red; font-size: 50px;");
+
+
+
+
