070ff4114a3034a54472b11358b9fa10eb6410dc
Kieu Duc <kieuduc223@gmail.com>
2026-02-06 16:44:27 +0700

    vd18 commit

---
+++ 18_Primitive-data-types/video18.js
@@ -0,0 +1,15 @@
+// Number
+const myAge = 22;
+let myScore = 9.1;
+
+// String
+const myAddress = "Ha Noi";
+
+// Undefined
+let myUndefined;
+console.log(myUndefined);
+
+// Null
+const testNull = null;
+console.log(testNull);
+
